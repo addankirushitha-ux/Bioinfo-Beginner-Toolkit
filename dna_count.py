7b34efa520968c92ac9390ed764d9e871cf4dffe
Rushitha Addanki <addankirushitha@gmail.com>
2026-04-19 12:12:25 +0530

    Add DNA counting script in dna_count.py

---
+++ dna_count.py
@@ -0,0 +1,5 @@
+# This is my first Bioinformatics script!
+sequence = "ATGCGATATGCCTAG"
+a_count = sequence.count("A")
+t_count = sequence.count("T")
+print("In this DNA string, A appears", a_count, "times and T appears", t_count, "times.")
